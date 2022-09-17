4bfc1fc78838a1e3818c0d80148496161db6e1af
slavapravda <104251403+slavapravda@users.noreply.github.com>
2022-09-17 22:18:52 +0000

    Deploying to gh-pages from @ slavapravda/goit-react-hw-08-phonebook@24e5c6fc719506a91af1260d6ee6c4c403340d2f 🚀

---
+++ static/js/680.71d5a430.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,o){o.r(n);var h=o(184);n.default=function(){return(0,h.jsx)("div",{children:(0,h.jsx)("h1",{children:"Hello!"})})}}}]);
+//# sourceMappingURL=680.71d5a430.chunk.js.map
